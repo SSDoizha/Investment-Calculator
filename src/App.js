780dce5ecf9b525fb249d26af873b8cae0131a03
SSDoizha <serhiidoizha@gmail.com>
2023-07-08 21:48:19 +0300

    Create a components

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import TotalInfo from "./Components/TotalTable/TotalInfo";
+import FormItem from "./Components/Userform/FormItem";
+import Header from "./Components/Header/Header";
+function App() {
+  const calculateHandler = (userInput) => {
+    const yearlyData = [];
+
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  };
+  return (
+    <div>
+      <Header />
+      <FormItem />;
+      <TotalInfo />;
+    </div>
+  );
+}
+
+export default App;
